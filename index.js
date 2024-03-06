98b7f791056749ccd9c22a4976a7525adf97169a
Marco Giuliani <thebaffo@gmail.com>
2024-03-06 16:37:44 +0100

    Add password check

--- index.js
+++ index.js
@@ -1,5 +1,54 @@
 // Password validation
-// TODO
+const button = document.getElementById("create-account-button");
+
+const isPasswordValid = () => {
+    const password = document.getElementById("password");
+    const confirmPassword = document.getElementById("confirm-password");
+
+    password.setCustomValidity('');
+    confirmPassword.setCustomValidity('')
+
+    if (!password || !password.value || password.value.trim() === '') {
+        password.setCustomValidity('Password should not be empty!');
+        password.reportValidity();
+
+        console.log('Password should not be empty!');
+
+        return false;
+    }
+
+    if (!confirmPassword || !confirmPassword.value || confirmPassword.value.trim() === '') {
+        confirmPassword.setCustomValidity('Confirm password should not be empty!');
+        confirmPassword.reportValidity();
+
+        console.log('Confirm password should not be empty!');
+
+        return false;
+    }
+    
+    if (password.value !== confirmPassword.value) {
+        password.setCustomValidity('Passwords do not match!')
+        password.reportValidity();
+
+        console.log('Passwords do not match!');
+
+        return false;
+    }
+
+    password.setCustomValidity('');
+    confirmPassword.setCustomValidity('');
+
+    return true;
+}
+
+
+button.addEventListener("click", (event) => {
+    if (!isPasswordValid()) {
+        event.preventDefault();
+    }
+
+    return true;
+});
 
 // Add error class when form fields are invalid
 const inputs = document.querySelectorAll("input.form-input");
